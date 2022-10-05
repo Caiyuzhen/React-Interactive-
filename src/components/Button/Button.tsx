5c840fef75dac17b909bc8a21b051791d424f0e3
Zen <caiyuzhen00@gmail.com>
2022-10-05 22:43:45 +0800

    update

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -14,12 +14,15 @@ type ButtonOwnProps<E extends React.ElementType = 'button'> ={
 }
 
 
+// type ButtonProps<E extends React.ElementType> = Omit<React.ComponentProps<E>, 'as' | 'children'>
+type ButtonProps<E extends React.ElementType> = ButtonOwnProps<E> & Omit<React.ComponentProps<E>, keyof ButtonOwnProps<E>>//Omit(XX 类型, 需要去重的 XX 类型), 拿到 ButtonOwnProps 里边的 keyName 以保障不会重名
+
+
 
 // 在范型内 extends XXX，相当于把 E 限制为 XXX 的类型, 在这里就是限制为(React 或 原生)元素的标签, 默认可以 = 'button' 类型
 export default function Button<E extends React.ElementType = 'button'>({
 	as, children, style, ...rest}: 	//{} ⚡️相当于直接解构赋值出 as 跟 children 两个变量！！然后直接定义它们的类型！: 冒号后面为类型
-	ButtonOwnProps<E> & Omit<React.ComponentProps<E>, 'as' | 'children'>){  //⚡️& 是联合声明，后面的这个类型要根据前面的类型类变化，就是用范型, 这里的 <E> 就是规范 as 的类型, Omit 是忽略重复的类型！
-
+	ButtonProps<E>){  //⚡️& 是联合声明，后面的这个类型要根据前面的类型类变化，就是用范型, 这里的 <E> 就是规范 as 的类型, Omit 是忽略重复的类型！
 
 	const Component = as ?? 'button' //🔥如果 as 不存在，默认就是 'button'
 
@@ -35,13 +38,11 @@ export default function Button<E extends React.ElementType = 'button'>({
 
 
 
-
 // ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 
 
-
-//用来规范 RoundedButton 内参数的 Props 类型！
+//用来规范 RoundedButton 内参数的 Props 类型！类似 interface 的作用！
 type RoundedButtonProps = {
 	size: 'large' | 'medium' | 'small'
 }
@@ -49,7 +50,6 @@ type RoundedButtonProps = {
 
 // 这个按钮可以传入上面那个组件内部定义的 as 属性，来改变组件的类型！
 export function RoundedButton({style, size='medium', ...rest}: React.ComponentProps<'button'> & RoundedButtonProps){//🔥用原生的 React.ComponentProps -> button 类型来规范参数！ & 表示再增加个联合类型！在外部定义！
-
 
 	//🔥🔥定义一个 style 变量，根据传入的 size 属性不同来改变样式！不用 styleComponent 的写法，本质都是一样的
 	const sizeStyle = {
@@ -63,7 +63,6 @@ export function RoundedButton({style, size='medium', ...rest}: React.ComponentPr
 			fontSize: 12
 		}
 	}[size] //🔥相当于 sizeStyle[size], 根据 size 来索引！
-
 
 	return (
 		<button 
